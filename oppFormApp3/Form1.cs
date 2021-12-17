46bff52ab56f78758c19768854744a59285e3163
Murat Karasah <mkarasah@gmail.com>
2021-12-17 14:48:18 +0300

    Add project files.

---
+++ oppFormApp3/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace oppFormApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Ogrenci o = new Ogrenci();
+            o.Ad = textBox1.Text;
+            o.Soyad = textBox2.Text;
+            o.Tc = Convert.ToInt64(textBox3.Text);
+            ListViewItem kayit = new ListViewItem();
+            kayit.Text = o.Tc.ToString();
+            kayit.SubItems.Add(o.Ad);
+            kayit.SubItems.Add(o.Soyad);
+            listView1.Items.Add(kayit);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            listView1.Items.Remove(listView1.SelectedItems[0]);
+        }
+    }
+}
---
+++ oppFormApp3/Ogrenci.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace oppFormApp3
+{
+    public class Ogrenci
+    {
+        //kapsülleme içerisine saklmış olduğmuz private bir field'ın önüne koyduğumuz bir porperty sayesinde get ve set kullanarak değer ataması yapabilir ve değer okuyabiliriz.
+        private string _Ad,_Soyad;
+        private long _Tc;
+        public string Ad 
+        { get {return _Ad;
+            }
+            set {_Ad=value.Substring(0,1).ToUpper()+value.Substring(1); 
+            }
+        }
+        public string Soyad { get {return _Soyad; } set {
+                string asd = "*";
+
+                //    int sayi = Convert.ToInt32(value.Length)-3;
+                for (int i = 0; i < value.Length - 2; i++)
+                {
+                      asd="*"+asd;
+                }
+                 _Soyad = asd+value.Substring(value.Length-2,2);
+
+                  }
+        }
+        public long Tc { get { return _Tc; } set { _Tc = Math.Abs(value);
+                  
+                    ; } }
+    }
+}
